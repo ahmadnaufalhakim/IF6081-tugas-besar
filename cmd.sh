38dcc4332b6670e56adc3dffafba7f2be750b312
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2023-12-05 13:03:35 +0700

    add comments in bash scripts for readability

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
+# Setting local system jobs (local CPU - no external clusters)
 train_cmd="run.pl"
 decode_cmd="run.pl"
--- path.sh
+++ path.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+
+# Defining Kaldi root directory (relative to this file)
 export KALDI_ROOT=`pwd`/../..
+
+# Enable SRILM
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+
+# Setting paths to useful tools
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
+
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
+
+# Variable needed for proper data sorting
 export LC_ALL=C
 export PYTHONUNBUFFERED=1
